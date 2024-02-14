f8502f51cc50ae8a80069bea8c05eda7b9be80b5
MingRuji <rujiphart.cha@student.mahidol.edu>
2024-02-15 02:29:26 +0700

    Rerun the TrialPyDriller.py and put the output folders from .gitignore

--- PythonFiles/pydriller/36890baf/fdf671856b260aca058e6595a96a7a0fba05454b/fdf671856b260aca058e6595a96a7a0fba05454b_pydriller_36890baf_20180322_110731_after_8.py
+++
@@ -1,9 +0,0 @@
-import abc
-from scm.scm_repository import SCMRepository
-from scm.persistence_mechanism import PersistenceMechanism
-
-
-class CommitVisitor(abc.ABC):
-    @abc.abstractmethod
-    def process(self, repo: SCMRepository, writer: PersistenceMechanism):
-        pass
